fa7345a819a8af3809011ea3f6a939b86e829ac1
Francis Moran <noramknarf@gmail.com>
2024-02-27 14:05:34 +0000

    lab 5 complete (again)

--- lab5-records/ada/playlist.adb
+++ lab5-records/ada/playlist.adb
@@ -14,37 +14,49 @@ procedure PlayList is
         Put (To_String (p.name));
     end Put_Person;
 
-    type Item is record
-        name        : Unbounded_String;
-        performer   : Person;
+    type variant is (PIECE, PAUSE);
+
+    type Item(item_variant : variant) is record
         length_secs : Float;
+        case item_variant is
+            when PIECE =>    
+                name        : Unbounded_String;
+                performer   : Person;
+            when PAUSE =>
+                null;
+        end case;
     end record;
 
     procedure Put_Item (i : Item) is
     begin
-        Put (To_String (i.name));
-        Put (" by ");
-        Put_Person (i.performer);
+        case i.item_variant is
+            when PIECE =>
+            Put (To_String (i.name));
+            Put (" by ");
+            Put_Person (i.performer);
+            when PAUSE =>
+            Put ("Pause ");
+        end case;
         Put (" (");
         Put (i.length_secs, aft => 1, exp => 0);
         Put ("s)");
     end Put_Item;
 
     piece1 : Item :=
-       (name => To_Unbounded_String ("Moonlight Sonata"),
+       (item_variant => PIECE, name => To_Unbounded_String ("Moonlight Sonata"),
         performer => (name => To_Unbounded_String ("Claudio Arrau")),
         length_secs => 17.0*60.0+26.0
        );
 
-    -- pause1 : Item :=
-    --    (
-    --     item_variant => PAUSE,
-    --     length_secs => 5.0
-    --    );
+    pause1 : Item :=
+       (
+        item_variant => PAUSE,
+        length_secs => 5.0
+       );
 
 begin
     Put_Item (piece1);
     Put_Line ("");
-    -- Put_Item(pause1);
-    -- Put_Line ("");
+    Put_Item(pause1);
+    Put_Line ("");
 end PlayList;
